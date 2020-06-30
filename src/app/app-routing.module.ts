a00070af34f2e72f2a9507e798ae64e85b4780ab
Maincho06 <mauricio_andre_98@hotmail.com>
2020-06-30 01:40:57 -0500

    Arreglos slidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/Base/login/login.component';
+import { MenuComponent } from './components/Base/menu/menu.component';
 
 
 const routes: Routes = [
